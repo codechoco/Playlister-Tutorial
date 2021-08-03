9d6c6c1a9a0f9a605545694f74bfe6bb3c8761d1
waltervvv <infabdu@gmail.com>
2021-08-03 08:03:08 +0200

    Les utilisateurs peuvent éditer, mettre à jour et détruire des listes de lecture

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_news.html')
 
-
+""" Insert action """
 # Note the methods parameter that explicitly tells the route that this is a POST
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -56,11 +56,42 @@ def playlists_submit():
     playlists.insert_one(playlist)
     return redirect("/")
 
+""" Show action """
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist)
+    return render_template('playlists_show.html', playlist=playlist,title = 'Show Playlist')
+
+""" Edition action """
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show a single playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist,title = 'Edit Playlist')
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+
+
 
 
 if __name__ == '__main__':
