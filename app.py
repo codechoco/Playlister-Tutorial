c70e826583af2a0ced127cc3838ce48367f33213
waltervvv <infabdu@gmail.com>
2021-08-03 04:37:57 +0200

    Ajout du model mongodb

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
+
 app = Flask(__name__)
+"""
 playlists  =  [ 
     {  'title' :  'Cat Videos' ,  'description' :  'Cats agissant bizarrement'  }, 
     {  'title' :  '80  Music' ,  'description' :  'Don  « arrête pas de croire ! »'  } 
 ]
+"""
 
 @app.route('/')
 def index():
     """Return homepage."""
     #return 'Hello, world!'
-    return render_template('home.html', msg='Flask is Cool!!')
+    #return render_template('home.html', msg='Flask is Cool!!')
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 @app.route('/playlists')
 def playlists_index():
